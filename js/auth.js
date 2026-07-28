3dace012cb1b86b66c7b9dc5af47faf491139a2c
malkiatTestingxperts <malkiat.singh@testingxperts.com>
2026-07-28 21:30:37 +0530

    updated changes

--- js/auth.js
+++ js/auth.js
@@ -114,7 +114,6 @@ function renderAuthUI() {
   } else {
     box.innerHTML = `
       <button class="signin-btn" onclick="signIn()">
-        <svg viewBox="0 0 23 23"><rect width="10" height="10" x="1" y="1" fill="#f25022"/><rect width="10" height="10" x="12" y="1" fill="#7fba00"/><rect width="10" height="10" x="1" y="12" fill="#00a4ef"/><rect width="10" height="10" x="12" y="12" fill="#ffb900"/></svg>
         Log In
       </button>${SSO_ENABLED ? "" : '<span class="sso-note">(setup pending)</span>'}`;
   }
